cf87fef89c8dc098192be6b00c63622adf8ecdb8
LuiFeAn <luisfernandogvv@gmail.com>
2023-10-09 12:34:40 -0300

    fix: Movendo processo "uncaughtExpection" para o topo

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,16 @@ const PORT_ = process.env.SERVER_WEB_PORT;
 import RouteHandler from './lib/route-handler';
 
 import GlobalErrorHandler from './lib/global-error-handler';
+
 import BodyParserHandler from './lib/body-parser-handler';
 
 const server = createServer(async ( req, res ) => {
 
+    process.on('uncaughtException', (err) => GlobalErrorHandler(err,req,res) )
+
     await BodyParserHandler(req,res);
 
     RouteHandler(req,res);
-
-    process.on('uncaughtException', (err) => GlobalErrorHandler(err,req,res) )
 
 });
 
