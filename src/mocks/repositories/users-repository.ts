42ada91fa79352602b6bdab8bcc560a8606cbdfe
Luis <luisfernandogvv@gmail.com>
2023-10-09 11:23:09 -0300

    feat: Criando mock de usuários, responsável por prover e realizar o "CRUD" deste recurso

---
+++ src/mocks/repositories/users-repository.ts
@@ -0,0 +1,48 @@
+export interface IUser {
+
+    readonly name: string
+
+    readonly age: number
+
+}
+
+class UsersRepository {
+
+    private users: IUser []
+
+    constructor(){
+       
+        this.users = [ 
+            {
+                name:'José Luis Cardoso',
+                age:30
+            },
+            {
+                name:'Marcelo Vinícius',
+                age:20
+            }
+        ]
+
+    }
+
+    insert(user: IUser){
+
+        this.users.push(user);
+
+    }
+
+    findAll(){
+
+        return this.users;
+
+    }
+
+    findOneByName(name: string){
+
+        return this.users.find( user => user.name === name );
+
+    }
+
+}
+
+export default new UsersRepository();
