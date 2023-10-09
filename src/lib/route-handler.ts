d7caf494852a2870bc58ad64079dbd38154dbbf6
Luis <luisfernandogvv@gmail.com>
2023-10-09 11:22:47 -0300

    feat: Criando route handler, responsável por lidar com toda a regra de negócio referente ao gerenciamento de rotas

---
+++ src/lib/route-handler.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "../interfaces/http";
+
+import { routes } from "../routes/main";
+
+export default function RouteHandler(req: Request, res: Response){
+
+    const route = routes.find( route => (
+        route.url === req.url && route.method === req.method 
+    ));
+
+    if( route ){
+
+        res.setHeader('Content-Type', 'application/json');
+        
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        route.handler(req,res);
+
+    }else{
+
+        res.writeHead(404);
+
+    }
+
+}
