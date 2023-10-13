edecf5ef9f0293f23514b98bb019759f51b75c1a
Luis <luisfernandogvv@gmail.com>
2023-10-13 08:37:27 -0300

    feat: Aplicando a utilização/injeção dos interceptors "StatusCodeHandler" e "JsonParserHandler" à todas as requisições

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,20 @@ import RouteHandler from './lib/route-handler';
 import GlobalErrorHandler from './lib/global-error-handler';
 
 import BodyParserHandler from './lib/body-parser-handler';
+
 import CorsHandler from './lib/cors-handler';
+
+import JsonParserHandler from './lib/json-parser-handler';
+
+import StatusCodeHandler from './lib/status-code-handler';
 
 const server = createServer(async ( req, res ) => {
 
     process.on('uncaughtException', (err) => GlobalErrorHandler(err,req,res) );
+
+    StatusCodeHandler(req,res);
+
+    JsonParserHandler(req,res);
 
     CorsHandler(req,res);
 
